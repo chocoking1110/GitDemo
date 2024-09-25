039f6549d3abad25862e0f8817c5f8ac18995fbb
chocoking <milantrinh1110@gmail.com>
2024-09-25 22:04:48 +0700

    this is git y commit

---
+++ src/RestTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class RestTest {
+	@Test
+	public void postJira() {
+		System.out.print("Jira post test 1");
+		System.out.print("Jira post test 2");
+		System.out.print("Jira post test 3");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.print("Twitter delete test");
+	}
+
+}
